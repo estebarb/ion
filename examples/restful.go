48db612ca5fe0e6e39c12021be33f44ecadf3574
Esteban RB <estebarb@gmail.com>
2015-02-02 13:11:11 -0600

    Updated restful example to use the new URLArgs. Also added instructions.

--- examples/restful.go
+++ examples/restful.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/estebarb/ion"
-	"github.com/gorilla/context"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
@@ -19,23 +17,31 @@ func (c restHandler) POST(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c restHandler) PUT(w http.ResponseWriter, r *http.Request) {
-	val := context.Get(r, ion.Urlargs).(httprouter.Params)
-	fmt.Fprint(w, "upsert thing as %v", val.ByName("id"))
+	val := ion.URLArgs(r, "id")
+	fmt.Fprint(w, "upsert thing as %v", val)
 }
 
 func (c restHandler) GET(w http.ResponseWriter, r *http.Request) {
-	val := context.Get(r, ion.Urlargs).(httprouter.Params)
-	fmt.Fprintf(w, "get thing %v", val.ByName("id"))
+	val := ion.URLArgs(r, "id")
+	fmt.Fprintf(w, "get thing %v", val)
 }
 
 func (c restHandler) DELETE(w http.ResponseWriter, r *http.Request) {
-	val := context.Get(r, ion.Urlargs).(httprouter.Params)
-	fmt.Fprintf(w, "delete thing %v", val.ByName("id"))
+	val := ion.URLArgs(r, "id")
+	fmt.Fprintf(w, "delete thing %v", val)
 }
 
 func main() {
 	r := ion.NewRouter()
 	var rest restHandler
+	r.GetFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, `Please try the following endpoints:
+	- GET       /test/      Lists the items
+	- POST      /test/      Post a new item
+	- GET       /test/:id   Gets a item
+	- DELETE    /test/:id   Deletes a item
+	- PUT       /test/:id   Updates a item`)
+	})
 	r.RegisterREST("/test/", rest)
 	http.ListenAndServe(":8080", r)
 }
