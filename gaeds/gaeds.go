b472deb73d4a43fa1f76dff880a47ea15e82dd0e
Esteban RB <estebarb@gmail.com>
2015-02-03 07:43:31 -0600

    More commands on gaeds

--- gaeds/gaeds.go
+++ gaeds/gaeds.go
@@ -19,10 +19,16 @@ func genMemcacheKey(bucket, key string) string {
 	return bucket + "_" + key
 }
 
+func GetAll(c appengine.Context, bucket string, values interface{}) ([]*datastore.Key, error){
+	q := datastore.NewQuery(bucket)
+	keys, err := q.GetAll(c, values)
+	return keys, err
+}
+
 // Saves a struct on the Datastore and Memcache. Example:
 //      err := gaeds.SaveAs(c, "framework", framework.Id, &fw)
-func SaveAs(c appengine.Context, bucket, key string, object interface{}) error {
-	ck := datastore.NewKey(c, bucket, key, 0, nil)
+func SaveAs(c appengine.Context, bucket, key string, object interface{}, parent *datastore.Key) error {
+	ck := datastore.NewKey(c, bucket, key, 0, parent)
 	_, err := datastore.Put(c, ck, object)
 	if err != nil {
 		return err
@@ -35,6 +41,24 @@ func SaveAs(c appengine.Context, bucket, key string, object interface{}) error {
 	memcache.Gob.Set(c, item)
 
 	return err
+}
+
+// Save a struct on the Datastore and Memcache, generating a new key.
+// Example:
+//      err := gaeds.Save(c, "framework", &fw)
+func Save(c appengine.Context, bucket string, object interface{}, parent *datastore.Key) (*datastore.Key, error) {
+	ck := datastore.NewIncompleteKey(c, bucket, parent)
+	key, err := datastore.Put(c, ck, object)
+	if err != nil {
+		return key, err
+	}
+
+	item := &memcache.Item{
+		Key:    genMemcacheKey(bucket, string(key.IntID())),
+		Object: object,
+	}
+	memcache.Gob.Set(c, item)
+	return key, err
 }
 
 // Gets a item from the datastore or memcache and saves it to
@@ -52,7 +76,7 @@ func Get(c appengine.Context, bucket, key string, object interface{}) error {
 	// If there is a problem then we try from datastore:
 	ck := datastore.NewKey(c, bucket, key, 0, nil)
 	err = datastore.Get(c, ck, object)
-	if err == nil {
+	if err == nil || err == datastore.ErrNoSuchEntity {
 		// Save the retrieved object in memcache
 		item := &memcache.Item{
 			Key:    mcKey,
