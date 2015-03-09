2f30239e8e874c09fb1692b0eb1a825f9c1c805a
Esteban RB <estebarb@gmail.com>
2015-03-08 18:23:59 -0600

    Added gae panic middleware

---
+++ iongae/middleware.go
@@ -0,0 +1,23 @@
+package iongae
+
+import (
+	"appengine"
+	"net/http"
+)
+
+
+func GAEPanicMiddleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				c := appengine.NewContext(r)
+				c.Errorf("[PANIC] %+v", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
