56f2d936840553c5b0b05fa50d1a98b15dfe90dc
Esteban RB <estebarb@gmail.com>
2016-08-14 17:24:59 -0600

    Requests context handling
    The package reqctx manages application context
    per http.Request.
    
    Signed-off-by: Esteban RB <estebarb@gmail.com>

--- components/reqctx/reqctx.go
+++ components/reqctx/reqctx.go
@@ -1,40 +1,71 @@
-
-// +build !go1.7
-
 // reqctx provides an uniform way to access request context.
 package reqctx
 
 import (
-	"golang.org/x/net/context"
 	"net/http"
 	"sync"
 )
 
-var contexts = make(map[*http.Request]*context.Context)
-var contextsLock sync.Mutex
-
-func Context(r *http.Request) *context.Context {
-	contextsLock.Lock()
-	defer contextsLock.Unlock()
-	return contexts[r]
+type State struct {
+	data map[string]interface{}
 }
 
-func ContextMiddleware(base *context.Context) {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(
-			func(w http.ResponseWriter, r *http.Request) {
-				ctx := base
-				contextsLock.Lock()
-				contexts[r] = ctx
-				contextsLock.Unlock()
-
-				defer func(){
-					contextsLock.Lock()
-					delete(contexts, r)
-					contextsLock.Unlock()
-				}()
-
-				next.ServeHTTP(w, r)
-			})
+func NewState() *State {
+	return &State{
+		data: make(map[string]interface{}),
 	}
+}
+
+func (s *State) GetAll() map[string]interface{} {
+	return s.data
+}
+
+func (s *State) Set(key string, value interface{}) {
+	s.data[key] = value
+}
+
+func (s *State) Get(key string) (interface{}, bool) {
+	v, ok := s.data[key]
+	return v, ok
+}
+
+func (s *State) Delete(key string) {
+	delete(s.data, key)
+}
+
+type StateContainer struct {
+	lock sync.RWMutex
+	data map[*http.Request](*State)
+}
+
+func NewStateContainer() *StateContainer {
+	return &StateContainer{
+		lock: sync.RWMutex{},
+		data: make(map[*http.Request](*State)),
+	}
+}
+
+func (s *StateContainer) GetState(r *http.Request) *State {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	state, ok := s.data[r]
+	if !ok {
+		state = NewState()
+		s.data[r] = state
+	}
+	return state
+}
+
+func (s *StateContainer) DestroyState(r *http.Request) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	delete(s.data, r)
+}
+
+func (s *StateContainer) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		s.GetState(r)
+		defer s.DestroyState(r)
+		next.ServeHTTP(w, r)
+	})
 }
