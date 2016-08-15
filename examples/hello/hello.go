60421e53f5e5bc295cb40f1d5a1843eabf1c9e2a
Esteban RB <estebarb@gmail.com>
2016-08-15 17:35:19 -0600

    Updated the hello example
    The hello example was updated to use the new router.
    
    Signed-off-by: Esteban RB <estebarb@gmail.com>

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/estebarb/ion"
 	"net/http"
+	"github.com/estebarb/ion/components/router"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	value := ion.URLArgs(r, "name")
-	if value != "" {
+type App struct {
+	*router.Router
+}
+
+func NewApp() *App {
+	app := &App{
+		Router: router.New(),
+	}
+	app.GetFunc("/", app.hello)
+	app.GetFunc("/:name", app.hello)
+	return app
+}
+
+func (app *App) hello(w http.ResponseWriter, r *http.Request) {
+	state := app.GetState(r)
+	value, exists := state.Get("name")
+	if exists {
 		fmt.Fprintf(w, "Hello, %v!", value)
 	} else {
 		fmt.Fprint(w, "Hello world!")
@@ -16,8 +30,5 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := ion.NewRouter()
-	r.GetFunc("/", hello)
-	r.GetFunc("/{name}", hello)
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":5000", NewApp())
 }
---
+++ examples/hello/hello_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"io/ioutil"
+)
+
+func compare(t *testing.T, path, expected string) {
+	ts := httptest.NewServer(NewApp())
+	defer ts.Close()
+	response, err := http.Get(ts.URL + path)
+	if err != nil {
+		t.Error(err)
+	}
+
+	body, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+
+	strbody := string(body)
+
+	if strbody != expected {
+		t.Errorf("Expected '%s', given '%s'", expected, strbody)
+	}
+}
+
+func TestNewApp(t *testing.T) {
+	compare(t, "/", "Hello world!")
+	compare(t, "/asdf", "Hello, asdf!")
+}
