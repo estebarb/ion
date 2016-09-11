9d1d76f03685d0b3dd4365d76de776cdeef91a19
Esteban RB <estebarb@gmail.com>
2016-09-11 11:59:33 -0600

    Expirable
    An expirable is like a future with a
    limited TTL. It is intended to be used
    in cases where a Future would be suitable,
    but the value fetched usually doesn't change.
    
    Signed-off-by: Esteban RB <estebarb@gmail.com>

---
+++ futures/expirable.go
@@ -0,0 +1,65 @@
+package futures
+
+import (
+	"time"
+	"sync"
+	"math/rand"
+)
+
+type Expirable struct {
+	sync.Mutex
+	timeout time.Duration
+	timestamp time.Time
+	value interface{}
+	generator func()interface{}
+}
+
+func NewExpirable(timeout time.Duration, generator func()interface{}) *Expirable{
+	return &Expirable{
+		timeout: timeout,
+		value: nil,
+		generator: generator,
+	}
+}
+
+func (e *Expirable) Read()interface{} {
+	return e.ReadFunc(e.generator)
+}
+
+func (e *Expirable) ReadFunc(f func()interface{})interface{} {
+	e.Lock()
+	defer e.Unlock()
+	if time.Since(e.timestamp) > e.timeout || e.value == nil {
+		e.value = f()
+		e.timestamp = time.Now()
+	}
+	return e.value
+}
+
+type ExpirablePool struct {
+	Expirables []*Expirable
+}
+
+func NewExpirablePool(timeout time.Duration, size int, generator func()interface{}) *ExpirablePool{
+	pool := &ExpirablePool{
+		Expirables: make([]*Expirable, size),
+	}
+	for i := range pool.Expirables {
+		pool.Expirables[i] = NewExpirable(timeout, generator)
+	}
+	return pool
+}
+
+func (p ExpirablePool) Pick() *Expirable {
+	return p.Expirables[rand.Intn(len(p.Expirables))]
+}
+
+func (p *ExpirablePool) Read()interface{} {
+	return p.Pick().Read()
+}
+
+func (p *ExpirablePool) ReadFunc(f func()interface{})interface{} {
+	return p.Pick().ReadFunc(f)
+}
+
+
